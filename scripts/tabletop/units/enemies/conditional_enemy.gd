9f9ad39f44af9b8dcd1bdd2dace0ccaf7f882905
Purpkin <guiejo2020@gmail.com>
2025-03-29 19:07:37 -0300

    ConditionalEnemy class

---
+++ scripts/tabletop/units/enemies/conditional_enemy.gd
@@ -0,0 +1,55 @@
+class_name ConditionalEnemy extends Unit
+## A Enemy class that runs a list of actions by order and conditions
+
+@export var min_ap: int = 1
+@export var max_ap: int = 6
+
+## A class that defines a pair of a behavior and a condition
+class ConditionalBehavior:
+    var apply: Callable ## Runs the actual behavior code if [member ConditionalBehavior.condition] is true
+    ## Executes a function that returns a boolean indicating if that action should be executed [br]
+    ## Note that this function should [b]NOT[/b] have side effects.
+    var condition: Callable 
+    
+    func _init(_condition: Callable, _apply: Callable) -> void:
+        apply = _apply
+        condition = _condition
+
+## Creates a [class ConditionalBehavior] and adds it to the list[br]
+## [param condition]: Must be a function taking [Tabletop] as a parameter and returning a [b]boolean[/b] and should [b]NOT[/b] have side effects[br]
+## [param apply]: Must be a function taking [Tabletop] as a parameter, returning a integer. Note that this callable [i]may[/i] be async
+func add_behavior(condition: Callable, apply: Callable):
+    _behaviors.append(ConditionalBehavior.new(condition, apply))
+
+var _behaviors: Array[ConditionalBehavior]
+
+## Called by the tabletop and runs the unit's turn loop
+func _on_turn(tabletop: Tabletop):
+    action_points = randi_range(min_ap, max_ap)
+
+    var could_execute_any := false
+
+    while true:
+        could_execute_any = false
+
+        for behavior in _behaviors:
+            var condition_result = behavior.condition.call(tabletop)
+            assert(condition_result is bool, "ERROR: ConditionalBehavior condition callable MUST return a BOOLEAN value")
+
+            if condition_result:
+                await behavior.apply.call(tabletop)
+                could_execute_any = true
+                break
+        
+        if not could_execute_any:
+            break
+        
+        
+
+
+
+            
+        
+
+
+
