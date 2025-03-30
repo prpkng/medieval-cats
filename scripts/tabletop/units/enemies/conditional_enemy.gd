9077261924765aa796f114fea363e7e03477d731
Purpkin <guiejo2020@gmail.com>
2025-03-30 17:46:23 -0300

    Conditional enemy for multiple actions on a single unit

--- scripts/tabletop/units/enemies/conditional_enemy.gd
+++ scripts/tabletop/units/enemies/conditional_enemy.gd
@@ -1,7 +1,7 @@
 class_name ConditionalEnemy extends Unit
 ## A Enemy class that runs a list of actions by order and conditions
 
-@export var min_ap: int = 1
+@export var min_ap: int = 2
 @export var max_ap: int = 6
 
 ## A class that defines a pair of a behavior and a condition
@@ -16,7 +16,8 @@ class ConditionalBehavior:
         condition = _condition
 
 ## Creates a [class ConditionalBehavior] and adds it to the list[br]
-## [param condition]: Must be a function taking [Tabletop] as a parameter and returning a [b]boolean[/b] and should [b]NOT[/b] have side effects[br]
+## [param condition]: Must be a function taking [Tabletop] as a parameter and returning a [b]boolean[/b] and should [b]NOT[/b] have side effects.
+## [i]NOTE: if null, will be threated as always true[br][/i]
 ## [param apply]: Must be a function taking [Tabletop] as a parameter, returning a integer. Note that this callable [i]may[/i] be async
 func add_behavior(condition: Callable, apply: Callable):
     _behaviors.append(ConditionalBehavior.new(condition, apply))
@@ -26,14 +27,19 @@ var _behaviors: Array[ConditionalBehavior]
 ## Called by the tabletop and runs the unit's turn loop
 func _on_turn(tabletop: Tabletop):
     action_points = randi_range(min_ap, max_ap)
+    print('ACTION_POINTS: %s' % action_points)
 
     var could_execute_any := false
 
     while true:
+        print("conditional turn, AP = %s" % action_points)
+        if action_points <= 0:
+            break
+
         could_execute_any = false
 
         for behavior in _behaviors:
-            var condition_result = behavior.condition.call(tabletop)
+            var condition_result = behavior.condition.call(tabletop) if behavior.condition != null else true
             assert(condition_result is bool, "ERROR: ConditionalBehavior condition callable MUST return a BOOLEAN value")
 
             if condition_result:
--- scripts/tabletop/units/enemies/melee_enemy.gd
+++ scripts/tabletop/units/enemies/melee_enemy.gd
@@ -1,24 +1,45 @@
-extends Unit
+extends ConditionalEnemy
 
+const ATTACK_RANGE := 4
+
+func _init() -> void:
+	add_behavior(can_attack, attack_behavior)
+	add_behavior(func(_tabletop): return true, move_behavior)
+
+func get_target_player(tabletop: Tabletop) -> Unit:
+	var players = tabletop.get_players()
+	players.sort_custom(sort_by_distance)
+
+	return players[0]
 
 func sort_by_distance(a: Unit, b: Unit):
 	return grid_position.distance_to(a.grid_position) < grid_position.distance_to(b.grid_position)
 
-## Called by the tabletop and runs the unit's turn loop
-func _on_turn(tabletop: Tabletop):
-	action_points = 4
+func can_attack(tabletop: Tabletop):
+	var player = get_target_player(tabletop)
+	return player.grid_position.distance_to(grid_position) <= ATTACK_RANGE
 
-	var players = tabletop.get_players()
-	players.sort_custom(sort_by_distance)
+func attack_behavior(tabletop: Tabletop):
+	print("Will Attack")
+	var target = get_target_player(tabletop)
+	
+	await get_tree().process_frame
+	
+	var action = AttackAction.new(target)
+	send_action.emit(action)
 
-	var target = players[0]
+	await action.completed
+
+
+func move_behavior(tabletop: Tabletop):
+	var target = get_target_player(tabletop)
 
 	var path = tabletop.get_grid_path(grid_position, target.grid_position)
-	if path.size() > action_points:
-		path.resize(action_points)
+	if path.size() > action_points+1:
+		path.resize(action_points+1)
 
 	print(target)
-	var dest = path[path.size()-1] / G.GRID_SIZE
+	var dest = path[path.size() - 1] / G.GRID_SIZE
 	print(dest)
 	await get_tree().process_frame
 
@@ -26,5 +47,3 @@ func _on_turn(tabletop: Tabletop):
 	send_action.emit(action)
 	
 	await action.completed
-	
-	send_action.emit(null)
